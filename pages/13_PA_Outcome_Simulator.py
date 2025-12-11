a6ca1edf503aeab84f685f78f1b5e9e327894f17
Pavan Kumar Polu <143832063+Pavankumar5459@users.noreply.github.com>
2025-12-11 12:51:51 -0600

    Delete pages directory

--- pages/13_PA_Outcome_Simulator.py
+++
@@ -1,5 +0,0 @@
-from utils.navigation import show_navbar
-show_navbar()
-import streamlit as st
-st.title("Prior Authorization Outcome Simulator")
-st.write("Simulate approval, denial or P2P outcomes.")
--- pages/14_Claims_Adjudication_Simulator.py
+++
@@ -1,5 +0,0 @@
-from utils.navigation import show_navbar
-show_navbar()
-import streamlit as st
-st.title("Claims Adjudication Simulator")
-st.write("Simulate claim payment, RTP, denial or partial pay.")
